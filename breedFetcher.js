681cf9e8641cf58faac87a94505ddf1c369a5eba
paarmin <parniaa@gmail.com>
2021-01-14 14:30:21 -0500

    allDone

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,24 @@
+
 const request = require('request');
-console.log(request);
+const BREED_NOT_FOUND = "the mentioned breed not found in our database";
+const uriPath = "https://api.thecatapi.com/v1/breeds/search?q=";
+
+const args = process.argv;
+let breedInput = args[2];
+breedInput = uriPath + breedInput;
+console.log(breedInput);
+
+request(breedInput, (error, response, body) => {
+  if (error === null) {
+    console.log(`This is the Error code form API: ${error}`);
+  }
+
+  //converting a string to an object
+  const data = JSON.parse(body);
+  if (data[0] !== undefined) {
+    console.log(data[0]);
+  } else {
+    console.log(BREED_NOT_FOUND);
+  }
+
+});
